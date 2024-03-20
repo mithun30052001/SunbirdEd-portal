5cba76a6c1c169bb318afe3efe979baa2da189ea
mithun30052001 <lmithun01@gmail.com>
2024-03-20 14:52:51 +0530

    Ed-1473 fix: quml player based changes

--- src/app/client/src/app/modules/player-helper/player-helper.module.ts
+++ src/app/client/src/app/modules/player-helper/player-helper.module.ts
@@ -16,17 +16,11 @@ import { FormsModule } from '@angular/forms';
 import { CommonConsumptionModule } from '@project-sunbird/common-consumption';
 import {QumlPlayerV2Service} from './service/quml-player-v2/quml-player-v2.service';
 import { QumlPlayerService } from './service/quml-player/quml-player.service';
-import { QumlLibraryModule as QsLibModule, QuestionCursor as QsCursor} from '@project-sunbird/sunbird-quml-player';
-import { QumlLibraryModule, QuestionCursor } from '@project-sunbird/sunbird-quml-player-v9';
-import { EditorCursor as QsEditorCursor } from '@project-sunbird/sunbird-questionset-editor';
 import { CslFrameworkService } from '../public/services/csl-framework/csl-framework.service';
 
 @NgModule({
   providers: [
-    { provide: QsEditorCursor, useExisting: QumlPlayerV2Service },
-    { provide: QsCursor, useExisting: QumlPlayerV2Service },
      CslFrameworkService,
-    { provide: QuestionCursor, useExisting: QumlPlayerService },
     // { provide: EditorCursor, useExisting: QumlPlayerService },
   ],
   imports: [
@@ -37,7 +31,6 @@ import { CslFrameworkService } from '../public/services/csl-framework/csl-framew
     CoreModule,
     FormsModule,
     CommonConsumptionModule,
-    QsLibModule,
     // QumlLibraryModule,
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
